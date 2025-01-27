25cdd7acccdf465516742f5f91279168c8d75835
dave-eska <dentes340@gmail.com>
2025-01-27 19:23:53 +0700

    im bevying babyyyy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
-fn main() {
-    let name: String = "dave".to_string();
-    println!("{}", name);
+use bevy::prelude::*;
+
+fn main(){
+    App::new()
+        .add_systems(Startup, setup)
+        .add_systems(Update, print_names)
+        .run();
+}
+
+pub fn setup(mut cmd: Commands){
+    cmd.spawn(Person{
+        name: "daveeska".to_string()
+    });
+    cmd.spawn(Person{
+        name: "yesy".to_string()
+    });
+}
+
+pub fn print_names(person_q: Query<&Person>){
+    for person in person_q.iter(){
+        println!("{}", person.name);
+    }
+}
+
+#[derive(Component)]
+pub struct Person{
+    pub name: String
 }
